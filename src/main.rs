80b61ea67b204a336c89e4adf24ddf71e4df2c90
160R <160R@protonmail.com>
2022-12-10 18:09:38 +0100

    implement persistence

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,41 @@
 
 
 use axum::{routing, extract};
-use std::net::SocketAddr;
+use redb::ReadableTable;
+use std::{
+    net::SocketAddr,
+    sync::Arc
+};
+
+
+#[derive(Clone)]
+struct State {
+    db: Arc<redb::Database>
+}
+
+const BLOGS: redb::TableDefinition<&str, &str> = redb::TableDefinition::new("blogs");
+
 
 
 #[tokio::main]
 async fn main() {
+    let db = unsafe {
+        redb::Database::create("blogs.redb")
+            .expect("Database Error")
+    };
+    let db = Arc::new(db);
 
-    let app: axum::Router<(), axum::body::Body> = axum::Router::new()
+    let state = State {
+        db,
+    };
+
+    let app: axum::Router<_, axum::body::Body> = axum::Router::new()
         .route("/", routing::get(get_about))
         .route("/blog", routing::get(get_articles))
         .route("/blog/:title", routing::post(create_article))
         .route("/blog/:title", routing::get(get_article))
-        .route("/image/:title", routing::get(get_article));
+        .route("/image/:title", routing::get(get_article))
+        .with_state(state);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     axum::Server::bind(&addr)
@@ -23,8 +46,21 @@ async fn main() {
 
 
 
-async fn get_articles() -> &'static str {
-    "Hello world"
+async fn get_articles(
+    extract::State(st): extract::State<State>,
+) -> String {
+    let db = Arc::clone(&st.db);
+    let txn = db.begin_read().expect("Txn read error");
+
+    let mut articles = vec![];
+    for t in txn.list_tables() {
+        for a in t {
+            articles.push(a)
+        }
+    }
+
+    let x = articles.join("\nhttp://127.0.0.1:300/blog/");
+    x
 }
 
 async fn get_about() -> &'static str {
@@ -33,13 +69,36 @@ async fn get_about() -> &'static str {
 
 async fn get_article(
     extract::Path(title): extract::Path<String>,
+    extract::State(st): extract::State<State>,
 ) -> String {
-    format!("Hello world: {title}")
+    let db = Arc::clone(&st.db);
+    let txn = db.begin_read().expect("Txn read error");
+    {
+        let table = txn.open_table(BLOGS).expect("Open read error");
+        if let Some(article) = table
+            .get(&title)
+            .expect("Get read error")
+        {
+            return String::from(article)
+        } else {
+            return title
+        }
+    }
 }
 
 async fn create_article(
     extract::Path(title): extract::Path<String>,
+    extract::State(st): extract::State<State>,
     payload: String,
 ) -> String {
+    let db = Arc::clone(&st.db);
+    let txn = db.begin_write().expect("Txn error");
+    {
+        let mut table = txn.open_table(BLOGS).expect("Open error");
+        table.insert(&title, &payload).expect("Insert error");
+    }
+    txn.commit().expect("Txn commit error");
+
+
     format!("title: {title}, payload: {payload}")
 }
