1713d8a2ac371a609054009efab76603204dea7a
160R <160R@protonmail.com>
2023-09-17 17:06:27 +0200

    comment main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,23 +17,32 @@ pub const ADDR: &str = "http://127.0.0.1:3000";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    // Don't use logging if `RUST_LOG` is unset
     let rust_log = std::env::var("RUST_LOG");
     if matches!(rust_log.as_deref(), Ok("trace")) {
         tracing_subscriber::fmt::init();
     }
 
+    // Connect to a database using `DATABASE_URL` from .env
     let database_url = &std::env::var("DATABASE_URL")
         .map_err(|_| "No DATABASE_URL set, check README.md")?;
     let mysql = sqlx::mysql::MySqlPool::connect(database_url).await?;
     let db = database::Database::new_migrate(mysql).await;
 
+    // Initialize application state
     let state = repository::ArticlesRepository::new(db);
 
+    // Set routes
     let app: axum::Router = axum::Router::new()
+
+        // entry points
         .route("/", routing::get(
             || async { Redirect::permanent("/blog") })
         )
         .route("/blog", routing::get(handlers::get_articles))
+
+        // navigation
         .route("/blog/next/:id", routing::get(handlers::next_article))
         .route("/blog/next/:id/", routing::get(
             |Path(id): Path<i64>| async move { Redirect::permanent(&format!("/blog/next/{id}")) })
@@ -42,6 +51,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .route("/blog/prev/:id/", routing::get(
             |Path(id): Path<i64>| async move { Redirect::permanent(&format!("/blog/prev/{id}")) })
         )
+        .route("/blog/:title", routing::get(handlers::get_article))
+        .route("/blog/:title/", routing::get(
+            |Path(title): Path<String>| async move { Redirect::permanent(&format!("/blog/{title}")) })
+        )
+
+        // manipulation
         .route("/blog/:title", routing::post(handlers::create_article))
         .route("/blog/:title/", routing::post(
             |Path(title): Path<String>| async move { Redirect::permanent(&format!("/blog/{title}")) })
@@ -50,17 +65,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .route("/blog/:title/", routing::delete(
             |Path(title): Path<String>| async move { Redirect::permanent(&format!("/blog/{title}")) })
         )
-        .route("/blog/:title", routing::get(handlers::get_article))
-        .route("/blog/:title/", routing::get(
-            |Path(title): Path<String>| async move { Redirect::permanent(&format!("/blog/{title}")) })
-        )
+
+        // host files or images
         .nest_service("/content", {
             let dir = services::ServeDir::new("content");
             routing::get_service(dir)
                 .handle_error(|e| async move { eprintln!("{e:?}") })
         })
+
+        // set application state
         .with_state(state);
 
+    // Start hosting
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
